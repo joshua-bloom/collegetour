7c33b71f763a7e122d482e35330823d7da07af9c
Sam <=>
2014-01-18 19:51:46 -0800

    main

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tour
+//
+//  Created by Samuel Clark on 3/24/11.
+//  Copyright 2011 College Visions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main (int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
